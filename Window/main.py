430b0c060926f928cb034a274f92b928b340ca35
Marcelo Henry <marcelohenry@gmail.com>
2023-04-23 17:22:54 -0300

    MiniGame

--- Window/main.py
+++ Window/main.py
@@ -11,7 +11,7 @@ fundo = pygame.image.load("Image/stage.png")
 fundo = pygame.transform.scale(fundo, (1280, 720))
 personagem = pygame.image.load("Image/boyfriend.png")
 window.blit(fundo, (0,0))
-window.blit(personagem, (720,300))
+window.blit(personagem, (730,300))
 loop = True
 while loop:
     for events in pygame.event.get():
