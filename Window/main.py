2f55425ca0aa29bddd329de703c19eb53c78ea7b
Marcelo Henry <marcelohenry@gmail.com>
2023-04-23 20:33:07 -0300

    Add Daddy_Dearest

--- Window/main.py
+++ Window/main.py
@@ -12,9 +12,18 @@ fundo = pygame.transform.scale(fundo, (1280, 720))
 boyfriend = pygame.image.load("Image/boyfriend.png")
 girlfriend = pygame.image.load("Image/girlfriend.gif")
 girlfriend = pygame.transform.scale(girlfriend, (330, 289))
+dadearest = pygame.image.load("Image/Daddy_Dearest.webp")
+dadearest = pygame.transform.scale(dadearest, (350,350))
+arrows1 = pygame.image.load("Image/arrows.png")
+arrows1 = pygame.transform.scale(arrows1, (300,100))
+arrows2 = pygame.image.load("Image/arrows.png")
+arrows2 = pygame.transform.scale(arrows2, (300,100))
 window.blit(fundo, (0,0))
 window.blit(girlfriend, (450, 200))
 window.blit(boyfriend, (730,300))
+window.blit(dadearest, (250,200))
+window.blit(arrows1, (720,100))
+window.blit(arrows2, (300,100))
 loop = True
 while loop:
     for events in pygame.event.get():
