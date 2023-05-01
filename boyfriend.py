3bf541b27784a7c78133803dccff7cc4d3cc369c
Marcelo Henry <marcelohenry@gmail.com>
2023-05-01 13:06:32 -0300

    Random Image

--- boyfriend.py
+++ boyfriend.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 
 def boyfriend(window):
-    boyfriend = pygame.image.load("Image/boyfriend.gif").convert_alpha()
+    images = "Image/boyfriend.gif", "Image/boyfriend_up.png", "Image/boyfriend_left.png", "Image/boyfriend_down.png", "Image/boyfriend_up.png"
+    image = random.choice(images)
+    boyfriend = pygame.image.load(image)
     boyfriend = pygame.transform.scale(boyfriend, (250, 242))
     window.blit(boyfriend, (730, 300))
