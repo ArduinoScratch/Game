612e635f91a74c62eb77f69aa7a28b4be252b858
Marcelo Henry <marcelohenry@gmail.com>
2023-05-13 12:09:12 -0300

    Bolinha

--- bolinha.py
+++ bolinha.py
@@ -1,27 +1,11 @@
 import threading
 import pygame
-
-def f():
-    x=80
-    y=80
-    xd=1
-    yd=1
-    while True:
-        window.fill((0,0,255))
-        pygame.draw.circle(window,(255,0,0,255),(x,y),80)
-        pygame.display.flip()
-        x=x+xd
-        y=y+yd
-        if x==(1000-80) or x==80:
-            xd=-xd
-        if y==(900-80) or y==80:
-            yd=-yd
-        pygame.time.delay(10)
+from girlfriend2 import girlfriend
 
 if __name__ == '__main__':
     pygame.init()
     window = pygame.display.set_mode((1000, 900))
-    t=threading.Thread(target = f)
+    t = threading.Thread(target=girlfriend(window))
     t.setDaemon(True)
     t.start()
     running = True
