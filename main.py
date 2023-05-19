ebb053a374d8bd21c5da4d98121bd78f829a6ea6
Marcelo Henry <marcelohenry@gmail.com>
2023-05-19 14:29:26 -0300

    Main

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ play_music()
 pygame.init()
 
 window = pygame.display.set_mode([1280, 720])
+label_result = pygame.font.SysFont("monospace", 50)
 
 pygame.display.set_caption("Friday Night Funkin - Dadbattle - Teclas: ASWD")
 y_up = 715
@@ -31,7 +32,7 @@ load_left1(window, y_left1)
 y_down1 = 715
 load_down1(window, y_down1)
 y_up1 = 715
-load_up1(window, y_up1)
+load_up1(window, y_up1, label_result)
 y_right1 = 715
 load_right1(window, y_right1)
 
@@ -45,8 +46,6 @@ arrows(window)
 # Loop
 loop = True
 while loop:
-
-    window.blit
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             pygame.quit()
@@ -54,8 +53,7 @@ while loop:
 
     # Atualizando Imagem de Fundo
     stage(window)
-
-    # Colocando os Personagens9
+    # Colocando os Personagens
     girlfriend(window)
     dadearest(window, mudar_foto_opponente_up, mudar_foto_opponente_right, mudar_foto_opponente_down, mudar_foto_opponente_left)
     arrows(window)
@@ -89,7 +87,10 @@ while loop:
     # Player
     y_left1 = load_left1(window, y_left1)
     y_down1 = load_down1(window, y_down1)
-    y_up1 = load_up1(window, y_up1)
+    y_up1 = load_up1(window, y_up1, label_result)
     y_right1 = load_right1(window, y_right1)
 
     pygame.display.update()
+
+
+
